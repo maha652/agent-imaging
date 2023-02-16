c5000dd99771feb8e9b5ba759f9f70cbe18af2a5
maha_graa <mahagraa.geniebio@gmail.com>
2023-02-16 18:03:52 +0100

    test_push

--- imaging/src/main/java/fr/gustaveroussy/imaging/ImagingApplication.java
+++ imaging/src/main/java/fr/gustaveroussy/imaging/ImagingApplication.java
@@ -1,7 +1,15 @@
+//package //
 package fr.gustaveroussy.imaging;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import com.pixelmed.dicom.DicomException;
+import com.pixelmed.dicom.DicomInputStream;
+import com.pixelmed.dicom.AttributeList;
+
+
+
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -19,6 +27,7 @@ import com.pixelmed.dicom.AttributeList;
 import com.pixelmed.dicom.AttributeTag;
 import com.pixelmed.dicom.DicomException;
 import com.pixelmed.dicom.TagFromName;
+import com.pixelmed.dicom.TransferSyntax;
 import com.pixelmed.display.SourceImage;
 import com.pixelmed.dicom.DicomInputStream;
 import com.pixelmed.dicom.PersonNameAttribute;
@@ -26,11 +35,17 @@ import com.pixelmed.dicom.PersonNameAttribute;
 import fr.gustaveroussy.imaging.services.DemoDicom;
 
 @SpringBootApplication
+
+// classe principale // 
+
+
 public class ImagingApplication implements CommandLineRunner {
 	 
 
 
 	public static final Logger logger = LoggerFactory.getLogger(ImagingApplication.class);
+
+	private static final Object[] String = null;
 	
 	private  AttributeList attributeList = new AttributeList() ;
 	
@@ -39,6 +54,19 @@ public class ImagingApplication implements CommandLineRunner {
 	
 	@Value("${path.to.write.file}")
 	private  String writeFile ;
+	
+	 @Value ("path.to.directory")
+		private String directoryName ;
+		
+
+	
+	
+	
+	
+	
+	
+	
+	// fonction principale main ( point d'entrée du programme ) //
 
  public static void main(String[] args) {
 		SpringApplication.run(ImagingApplication.class, args);
@@ -62,11 +90,54 @@ public class ImagingApplication implements CommandLineRunner {
 	    
 	     
 	     }
+
 	
 	
+// acceder à une liste de dicom dans un fichier //	
+	
+/* Cet exemple utilise la méthode listFiles() de la classe File pour obtenir une liste de tous les fichiers DICOM dans un dossier donné. 
+ * La boucle for parcourt ensuite chaque fichier, lit son contenu en utilisant DicomInputStream et ajoute les attributs à une liste d'AttributeList.
+ */
 	
 	
+ public void Trouver(String[] args) {
+	 
+	 File directory = new File(directoryName); 
+	 
+	File[] files = directory.listFiles((dir , name) -> name.endsWith(".dcm")); 
+
+
+	ArrayList<AttributeList> attributeLists = new ArrayList<>();
+
+	for (File file : files) {
+	    try {
+	        DicomInputStream din = new DicomInputStream(file);
+	        AttributeList attributeList = new AttributeList();
+	        attributeList.read(din);
+	        din.close();
+	        attributeLists.add(attributeList);
+	    } catch (IOException | DicomException e) {
+	        e.printStackTrace(); 
+	    }
+	    
+	    
+	    // copier les DICOM , avoir le file des DICOM copiés , et travailler sur ces copies//
+	    
+	  // Accédez aux données DICOM ( deja copiés)  ici en utilisant attributeLists , modifier(anonymiser et transférer les copies) //
 	
+ }
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // methode_lecture_d'un_seul_dicom_
 		 
 	    private   Map<String, String> readMetadata() throws DicomException, IOException {
 	        Map<String, String> metaData = new LinkedHashMap<>();
@@ -95,35 +166,40 @@ public class ImagingApplication implements CommandLineRunner {
 	        metaData.put("PatientSpeciesCodeSequence", getTagInformation(TagFromName.PatientSpeciesCodeSequence ));
 	        metaData.put("PatientSpeciesDescription ", getTagInformation(TagFromName.PatientSpeciesDescription ));
 	        metaData.put("PatientState", getTagInformation(TagFromName.PatientState ));
-	        metaData.put("PatientWeight", getTagInformation(TagFromName.PatientAddress  ));
-	        
+	        metaData.put("PatientWeight", getTagInformation(TagFromName.PatientWeight  ));
+	       
 	     
 	       
-	        attributeList.entrySet().forEach(e -> {
+	     /*   attributeList.entrySet().forEach(e -> {
 	        	System.out.println(e.getKey().toString());
 	        	System.out.println(e.getValue().toString());
-	        });
+	        });   */
 	        
-
+// modifier la variable au niveau du DICOM d'origine //
 	    	   Attribute patientNameAttr = new PersonNameAttribute(TagFromName.PatientName);
 		        patientNameAttr.addValue("ANONYMIZED");
 		        attributeList.put(TagFromName.PatientName,patientNameAttr);
-		       
 		        
-		   
-			     
-		   //return  (Map<String, String>) patientNameAttr ; //
-	      
-	      return metaData;  
-	   
-	    }
-	    
-	    
-	     
-	    
-	    
-	    
-	    
+		     // Save anonymized DICOM file
+			    File anonymizedFile = new File("C:\\Users\\m_graa\\Desktop\\stage_bioinfo\\anonym.txt");
+			    
+			    
+			    
+			    attributeList.write(anonymizedFile  ,  TransferSyntax.ExplicitVRLittleEndian, true, true);
+		        
+		        
+		        
+		        
+		        
+		        
+		        
+		     
+   	
+		        return metaData; 
+		      
+		        }     
+	 
+		
 	        private  String getTagInformation(AttributeTag tag) {
 		        return Attribute.getDelimitedStringValuesOrDefault(attributeList, tag, "NOT FOUND");
 	
@@ -131,11 +207,7 @@ public class ImagingApplication implements CommandLineRunner {
 	        }	
 
 	        
-	            
-	    
-	        
-	        
-	        
+	       	        
 	}
 
 
